40418fadfc58e5921df1812cd0f2471abf1baf6b
Shravan Patel <patelshravan24@gmail.com>
2022-05-21 21:30:45 +0530

    removed active class

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = (props) => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/">
+              <NavLink className="nav-link" aria-current="page" to="/">
                 Home
               </NavLink>
             </li>
